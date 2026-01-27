d816a0a68e2425af93bbbc23bab6fa06e491d2f3
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:29:04 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-paypal/main.go
+++ cmd/baton-paypal/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/conductorone/baton-paypal/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"github.com/spf13/viper"
-	"github.com/conductorone/baton-paypal/pkg/connector"
 	"go.uber.org/zap"
 )
 
@@ -27,6 +28,7 @@ func main() {
 		field.Configuration{
 			Fields: ConfigurationFields,
 		},
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
